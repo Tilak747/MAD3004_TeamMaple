ee44ace0dc00b775947ea5e95c8e2b5c3edcece6
Tilak <tilakacharya903@gmail.com>
2022-10-02 20:28:14 -0400

    files creation

---
+++ MAD3004_TeamMaple/Work Space/Part 2/Vehicle.swift
@@ -0,0 +1,8 @@
+//
+//  Vehicle.swift
+//  MAD3004_TeamMaple
+//
+//  Created by Tilak Acharya on 2022-10-02.
+//
+
+import Foundation
---
+++ MAD3004_TeamMaple/Work Space/Part 3/Permanent.swift
@@ -0,0 +1,8 @@
+//
+//  Permanent.swift
+//  MAD3004_TeamMaple
+//
+//  Created by Tilak Acharya on 2022-10-02.
+//
+
+import Foundation
