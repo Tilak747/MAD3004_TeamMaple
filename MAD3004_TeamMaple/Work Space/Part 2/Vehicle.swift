e9ed053346315d239ec92235c80694894fd5fdc4
pavitkalra150 <c0868150@mylambton.ca>
2022-10-05 18:59:31 -0400

    adding get methods for vehicle class

--- MAD3004_TeamMaple/Work Space/Part 2/Vehicle.swift
+++ MAD3004_TeamMaple/Work Space/Part 2/Vehicle.swift
@@ -12,4 +12,19 @@ class Vehicle {
     private var _plate: String = "";
     private var _color: String = "";
     private var _category: String = "";
+
+    //getters for vehicle
+    var make:String{
+        get{return _make}
+    }
+    var plate:String{
+        get{return _plate}
+    }
+    var color:String{
+        get{return _color}
+    }
+    var category:String{
+        get{return _category}
+    }
+
 }
