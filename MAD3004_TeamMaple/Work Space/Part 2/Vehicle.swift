82595ffa6d57576580dcc781d40af117fd30eb9f
pavitkalra150 <c0868150@mylambton.ca>
2022-10-05 18:56:04 -0400

    defining variables for vehicle class

--- MAD3004_TeamMaple/Work Space/Part 2/Vehicle.swift
+++ MAD3004_TeamMaple/Work Space/Part 2/Vehicle.swift
@@ -7,4 +7,9 @@ import Foundation
 
 class Vehicle {
     
+    //defining the variables
+    private var _make: String = "";
+    private var _plate: String = "";
+    private var _color: String = "";
+    private var _category: String = "";
 }
