9fd9d5b1929e16d2424e7b70218aa2aa875c9c86
Alice203 <c0874203@mylambton.ca>
2022-10-05 17:56:00 -0400

    Define setter and getter for Employee
    Define setter and getter for Employee

--- MAD3004_TeamMaple/Work Space/Part 1/Employee.swift
+++ MAD3004_TeamMaple/Work Space/Part 1/Employee.swift
@@ -14,4 +14,42 @@ class Employee {
     private var _monthlyIncome : Double
     private var _rate : Int = 100;
     private var _employeeVehicle : Vehicle;
+    
+    //defining setter & getter
+    var name: String {
+        get { return _name }
+        set { _name = newValue }
+    }
+    
+    var birthYear: Int {
+        get { return _birthYear }
+        set { _birthYear = newValue }
+    }
+    
+    var age: Int {
+        get { return _age }
+        set { _age = newValue }
+    }
+    
+    var monthlyIncome: Double {
+        get { return _monthlyIncome }
+        set { _monthlyIncome = newValue }
+    }
+    
+    var rate: Int {
+        get {
+            if(self.rate < 10){
+                return 10;
+            }else if(self.rate > 100){
+                return 100;
+            }
+            return _rate
+        }
+        set { _rate = newValue }
+    }
+    
+    
+    var employeeVehicle: Vehicle {
+        get { return _employeeVehicle }
+    }
 }
