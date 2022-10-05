87ed4d483d66a8b4b52fc5818c385ce6e0d1be24
Tilak <tilakacharya903@gmail.com>
2022-10-05 18:14:17 -0400

    initialisations for "Manager"

--- MAD3004_TeamMaple/Work Space/Part 1/Manager.swift
+++ MAD3004_TeamMaple/Work Space/Part 1/Manager.swift
@@ -20,6 +20,34 @@ class Manager: Employee {
         set { _nbClients = newValue }
     }
     
+    
+    init(name: String, birthYear: Int,nbClients: Int,nbTravelDays: Int) {
+        super.init(name: name, birthYear: birthYear)
+        self._nbTravelDays = nbTravelDays;
+        self._nbClients = nbClients;
+        printMessage()
+    }
+    init(name: String, birthYear: Int,nbClients: Int,nbTravelDays: Int,rate:Int) {
+        super.init(name: name, birthYear: birthYear, rate: rate)
+        self._nbTravelDays = nbTravelDays;
+        self._nbClients = nbClients;
+        printMessage()
+    }
+    init(name: String, birthYear: Int,nbClients: Int,nbTravelDays: Int, employeeVehicle: Vehicle) {
+        super.init(name: name, birthYear: birthYear,employeeVehicle: employeeVehicle)
+        self._nbTravelDays = nbTravelDays;
+        self._nbClients = nbClients;
+        printMessage()
+    }
+    init(name: String, birthYear: Int,nbClients: Int,nbTravelDays: Int,rate:Int, employeeVehicle: Vehicle) {
+        super.init(name: name, birthYear: birthYear,rate: rate,employeeVehicle: employeeVehicle)
+        self._nbTravelDays = nbTravelDays;
+        self._nbClients = nbClients;
+        printMessage()
+    }
+
+    
+    
     func printMessage() {
         printData("We have a new employee: \(name), a manager.")
 //        print("We have a new employee: \(name), a manager");
