c23075816788d49542f9238d5fbab5974bb655ab
mehrasimran768 <115096378+mehrasimran768@users.noreply.github.com>
2022-10-05 19:41:20 -0400

    Initialisations for car

--- MAD3004_TeamMaple/Work Space/Part 2/Car.swift
+++ MAD3004_TeamMaple/Work Space/Part 2/Car.swift
@@ -9,4 +9,17 @@ class Car: Vehicle {
     private var _gear:String = "";
     private var _type:String = "";
 
+ //initialisation
+
+    init(gear:String,type:String ){
+        super.init()
+        self._gear = gear
+        self._type = type
+    }
+init(make:String,plate:String,color:String,category:String,gear:String,type:String){
+
+    super.init(_male :make,_plate :plate,_color :color,_category :category)
+    self._gear = gear
+    self._type = type
+}
 }
