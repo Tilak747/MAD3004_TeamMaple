1d65e54b16af83eca2add8a1be299a7e25d4cce1
0871063 <c0871063@mylambton.ca>
2022-10-05 18:30:44 -0400

    Tester Class - Variable Declaration

--- MAD3004_TeamMaple/Work Space/Part 1/Programmer.swift
+++ MAD3004_TeamMaple/Work Space/Part 1/Programmer.swift
@@ -6,5 +6,5 @@
 import Foundation
 
 class Programmer: Employee {
-    
+
 }
--- MAD3004_TeamMaple/Work Space/Part 1/Tester.swift
+++ MAD3004_TeamMaple/Work Space/Part 1/Tester.swift
@@ -6,5 +6,16 @@
 import Foundation
 
 class Tester: Employee {
+    private var _nbBugs: Int=0;
+    
+    var nbBugs: Int {
+            get { return _nbBugs }
+            set { _nbBugs = newValue }
+        }
+
+    func printMessage() {
+        printData("We have a new employee: \(name), a tester")
+//        print("We have a new employee: \(name), a tester");
+    }
     
 }
