35721fab3aac07e668410e66be03c9cb3438e6ba
mehrasimran768 <115096378+mehrasimran768@users.noreply.github.com>
2022-10-05 19:51:03 -0400

    initialisation for motorcycle

--- MAD3004_TeamMaple/Work Space/Part 2/Motorcycle.swift
+++ MAD3004_TeamMaple/Work Space/Part 2/Motorcycle.swift
@@ -8,4 +8,15 @@ import Foundation
 class Motorcycle: Vehicle {
     //defining variable
     private var _sidecar:Bool = false;
+
+    //initialisations
+    init(sidecar:Bool){
+        super.init()
+        self _sidecar = sidecar 
+    }
+    init(make:String, plate:String, color:String ,category:String ,sidecar:Bool){
+
+        super.init(_make:String,_plate:String,_color:String ,_category:String ,_sidecar:Bool)
+        self._sidecar=sidecar
+    }
 }
