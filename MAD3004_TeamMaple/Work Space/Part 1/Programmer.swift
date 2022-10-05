18e44ca865e4bbdd422065ce0e06ce0ee1bdc6a9
0871063 <c0871063@mylambton.ca>
2022-10-05 18:31:53 -0400

    Programmer Class - Variable Declaration

--- MAD3004_TeamMaple/Work Space/Part 1/Programmer.swift
+++ MAD3004_TeamMaple/Work Space/Part 1/Programmer.swift
@@ -7,4 +7,15 @@ import Foundation
 
 class Programmer: Employee {
 
+    private var _nbProjects: Int=0;
+
+    var nbProjects: Int {
+            get { return _nbProjects }
+            set { _nbProjects = newValue }
+        }
+
+    func printMessage() {
+            print("We have a new employee: \(name), a programmer");
+        }
+
 }
