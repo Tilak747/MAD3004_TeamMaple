8dbd7e861e75b098471a5ab5c48846839a764e57
Alice203 <c0874203@mylambton.ca>
2022-10-05 17:59:37 -0400

    Create extension for Date
    Create extension for Date

---
+++ MAD3004_TeamMaple/Work Space/Part 1/DateExtension.swift
@@ -0,0 +1,38 @@
+//
+//  DateExtension.swift
+//  Project_MAD3004_TeamMaple
+//
+
+import Foundation
+
+extension Date {
+
+    //An integer representation of age from the date object.
+    var age: Int {
+        get {
+            let now = Date()
+            let calendar = Calendar.current
+
+            let ageComponents = calendar.dateComponents([.year], from: self, to: now)
+            let age = ageComponents.year!
+            return age
+        }
+    }
+
+    init(year: Int, month: Int, day: Int) {
+        var dateComponent = DateComponents()
+        dateComponent.year = year
+        dateComponent.month = month
+        dateComponent.day = day
+
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = TimeZone(secondsFromGMT: 0)!
+        if let date = calendar.date(from: dateComponent) {
+            self.init(timeInterval: 0, since: date)
+        } else {
+            fatalError("Date component values were invalid.")
+        }
+    }
+
+}
+
