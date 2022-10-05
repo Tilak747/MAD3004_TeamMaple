ec4244142de7388fc74ccee817092e027c41f7e4
Tilak <tilakacharya903@gmail.com>
2022-10-05 18:06:23 -0400

    getter and setter for "Manager"

--- MAD3004_TeamMaple/Work Space/Part 1/Manager.swift
+++ MAD3004_TeamMaple/Work Space/Part 1/Manager.swift
@@ -10,4 +10,14 @@ class Manager: Employee {
     private var _nbTravelDays: Int=0;
     private var _nbClients : Int=0;
     
+    var nbTravelDays: Int {
+        get { return _nbTravelDays }
+        set { _nbTravelDays = newValue }
+    }
+
+    var nbClients: Int {
+        get { return _nbClients }
+        set { _nbClients = newValue }
+    }
+
 }
