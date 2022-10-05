4eac37487b91807505b907b2b8275f29881d4874
Alice203 <c0874203@mylambton.ca>
2022-10-05 17:55:03 -0400

    Define variables for Employee
    Define variables for Employee

--- MAD3004_TeamMaple/Work Space/Part 1/Employee.swift
+++ MAD3004_TeamMaple/Work Space/Part 1/Employee.swift
@@ -7,4 +7,11 @@ import Foundation
 
 class Employee {
     
+    //necessary variables
+    private var _name : String
+    private var _birthYear : Int
+    private var _age : Int;
+    private var _monthlyIncome : Double
+    private var _rate : Int = 100;
+    private var _employeeVehicle : Vehicle;
 }
