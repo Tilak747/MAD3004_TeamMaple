a5f7eaee760eb275714002020d45ee231b8bcd9e
Alice203 <c0874203@mylambton.ca>
2022-10-05 18:01:21 -0400

    Define function to calculate age and another function to print message
    Define function to calculate age and another function to print message

--- MAD3004_TeamMaple/Work Space/Part 1/DateExtension.swift
+++ MAD3004_TeamMaple/Work Space/Part 1/DateExtension.swift
@@ -6,25 +6,25 @@
 import Foundation
 
 extension Date {
-
+    
     //An integer representation of age from the date object.
     var age: Int {
         get {
             let now = Date()
             let calendar = Calendar.current
-
+            
             let ageComponents = calendar.dateComponents([.year], from: self, to: now)
             let age = ageComponents.year!
             return age
         }
     }
-
+    
     init(year: Int, month: Int, day: Int) {
         var dateComponent = DateComponents()
         dateComponent.year = year
         dateComponent.month = month
         dateComponent.day = day
-
+        
         var calendar = Calendar(identifier: .gregorian)
         calendar.timeZone = TimeZone(secondsFromGMT: 0)!
         if let date = calendar.date(from: dateComponent) {
@@ -33,6 +33,17 @@ extension Date {
             fatalError("Date component values were invalid.")
         }
     }
-
+    
+    func calculateAge(birthYear: Int) -> Int {
+        
+        let currentYear = 2022
+        let age = currentYear - birthYear
+        
+        return age
+    }
+    
+    func printData(_ msg :String = "We have a new employee"){
+        print(msg)
+    }
 }
 
