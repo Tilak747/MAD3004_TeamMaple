14d72c0a3183f14690ee074c83b4f7d75a4f30a4
Tilak <c0868747@mylambton.ca>
2022-10-07 15:03:28 -0400

    getter and setter for permanent employee

--- MAD3004_TeamMaple/Work Space/Part 3/Permanent.swift
+++ MAD3004_TeamMaple/Work Space/Part 3/Permanent.swift
@@ -13,4 +13,29 @@ class Permanent:Contract {
     private var _bonusPerChildPerMonth: Int = 0
     private var _accumulatedDays: Int = 0
     
+    var nbChildren: Int {
+        get { return _nbChildren }
+        set { _nbChildren = newValue }
+    }
+    
+    var married: Bool {
+        get { return _married }
+        set { _married = newValue }
+    }
+    
+    var monthlySalary: Int {
+        get { return _monthlySalary }
+        set { _monthlySalary = newValue }
+    }
+    
+    var bonusPerChildPerMonth: Int {
+        get { return _bonusPerChildPerMonth }
+        set { _bonusPerChildPerMonth = newValue }
+    }
+    
+    var accumulatedDays: Int {
+        get { return _accumulatedDays }
+        set { _accumulatedDays = newValue }
+    }
+
 }
