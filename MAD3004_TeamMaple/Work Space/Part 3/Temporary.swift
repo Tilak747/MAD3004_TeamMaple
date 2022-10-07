889c3b2400cd47ae01c42c0e580794219a3f7886
mehrasimran768 <115096378+mehrasimran768@users.noreply.github.com>
2022-10-07 15:25:08 -0400

    defining getter and setter for Temporary employee

--- MAD3004_TeamMaple/Work Space/Part 3/Temporary.swift
+++ MAD3004_TeamMaple/Work Space/Part 3/Temporary.swift
@@ -9,4 +9,15 @@ class Temporary:Contract {
     
     private var _hourlySalary:Int=0
     private var _accumulatedHours:Int=0
+
+    var _hourlySalary:Int{
+       get{return _hourlySalary}
+       set{_hourlySalary=newValue}
+
+    }
+
+    var _accumulatedHours:Int{
+        get{return _accumulatedHours}
+        set{_accumulatedHours=newValue}
+    }
 }
